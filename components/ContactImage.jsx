2dae1f067556600cb6486f477a2420d304235efc
VitorAVPereira <amaralvp123@gmail.com>
2024-02-28 20:08:40 -0300

    fix: margin form

--- components/ContactImage.jsx
+++ components/ContactImage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './styles/formCard.module.css'
 import { Contact } from './FormContact'
 
 const ContactImage = () => {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-cover img-custom'>
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
-        <div className='p-5 text-white z-[2] mt-auto mb-auto'>
+        <div className={styles.mobileClass}>
           <Contact />
         </div>
     </div>
